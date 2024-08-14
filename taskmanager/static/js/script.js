357164f6d334087b959d2f924799eb3ebc6d8126
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-14 20:18:11 +0000

    create add task function and add task page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,8 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialized
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
-    
+
     // modal initialized
     var modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
   });
