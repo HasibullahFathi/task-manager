b3abdc92763137958349e29096dfdb6a19980131
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-14 19:41:50 +0000

    add a conformation message with modal to give a warning

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialized
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
+    
+    // modal initialized
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
   });
